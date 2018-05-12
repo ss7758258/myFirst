a907b5e9abb6aafc981ce880b2d7eebaa7f924a8
JackPeng <keddy8@126.com>
2018-05-12 20:56:24 +0800

    init

--- kafka_api/src/main/java/com/yeting/web/service/CollectionService.java
+++
@@ -1,9 +0,0 @@
-package com.yeting.web.service;
-
-import com.yeting.framework.common.base.BaseService;
-import com.yeting.web.mapper.entity.Collection;
-
-public interface CollectionService extends BaseService<Collection> {
-
-    void deleteByArtIdAndOpenId(long l, String openId);
-}
--- kafka_api/src/main/java/com/yeting/web/service/WeixinUserService.java
+++
@@ -1,8 +0,0 @@
-package com.yeting.web.service;
-
-import com.yeting.framework.common.base.BaseService;
-import com.yeting.web.mapper.entity.WeixinUser;
-
-public interface WeixinUserService extends BaseService<WeixinUser> {
-
-}
