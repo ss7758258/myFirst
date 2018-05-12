ec4b8265d797611e43b5c2ecd88b37691743a5e1
JackPeng <keddy8@126.com>
2018-05-12 20:54:02 +0800

    init

--- xz_api/src/main/java/com/xz/web/utils/IdUtil.java
+++ xz_api/src/main/java/com/xz/web/utils/IdUtil.java
@@ -15,6 +15,9 @@ public class IdUtil {
 	 * 获取UUID
 	 * @return
 	 */
+	public static String getDefaultUuid() {
+		return UUID.randomUUID().toString().replace("-", "");
+	}
 	public static String getUuid() {
 		return UUID.randomUUID().toString().replace("-", "");
 	}
