8767ab0cc0d41cc9e05e5ed7765d1d28501ab94b
JackPeng <keddy8@126.com>
2018-05-15 18:51:58 +0800

    init

--- xz_api/src/main/java/com/xz/web/controller/EverydayQianController.java
+++ xz_api/src/main/java/com/xz/web/controller/EverydayQianController.java
@@ -439,10 +439,36 @@ public class EverydayQianController extends BaseController {
             String useridStr = redisService.get("openId-:" + weixin.getOpenId());
             Long userId = Long.valueOf(useridStr);
             X511 x511 = new X511();
-            BeanUtil.copyProperties(data, x511);
+            BeanUtil.copyProperties(data, x511,true);
             if(data.getUserId()==userId)
             {
                 x511.setIsMyQian(1);
+            }
+            if (StringUtil.isNotEmpty(data.getFriendOpenId1())) {
+                if(weixin.getOpenId().equals(data.getFriendOpenId1()))
+                {
+                    x511.setAlreadyOpen(1);
+                }
+            } else if (StringUtil.isNotEmpty(data.getFriendOpenId2())) {
+                if(weixin.getOpenId().equals(data.getFriendOpenId2()))
+                {
+                    x511.setAlreadyOpen(2);
+                }
+            } else if (StringUtil.isNotEmpty(data.getFriendOpenId3())) {
+                if(weixin.getOpenId().equals(data.getFriendOpenId3()))
+                {
+                    x511.setAlreadyOpen(3);
+                }
+            } else if (StringUtil.isNotEmpty(data.getFriendOpenId4())) {
+                if(weixin.getOpenId().equals(data.getFriendOpenId4()))
+                {
+                    x511.setAlreadyOpen(4);
+                }
+            } else if (StringUtil.isNotEmpty(data.getFriendOpenId5())) {
+                if(weixin.getOpenId().equals(data.getFriendOpenId5()))
+                {
+                    x511.setAlreadyOpen(5);
+                }
             }
             String ownerOpenId = weixin.getOpenId();
             if (StringUtil.isNotEmpty(ownerOpenId)) {
