445edaf8310c60fc921dc754ddee9177646e66a4
JackPeng <keddy8@126.com>
2018-05-17 14:45:13 +0800

    init

--- xz_api/src/main/java/com/xz/web/mapper/ext/EverydayWordsMapperExt.java
+++ xz_api/src/main/java/com/xz/web/mapper/ext/EverydayWordsMapperExt.java
@@ -12,7 +12,7 @@ public interface EverydayWordsMapperExt {
 
     @Select({
             "select id, prev_pic as prevPic, speech, publish_time as currentDate from ti_yan_list",
-            " where (update_timestamp) <= now() and constellation_id = #{0} ORDER BY update_timestamp desc LIMIT 0 ,1"
+            " where publish_status = '1' and (update_timestamp) <= now() and constellation_id = #{0} ORDER BY update_timestamp desc LIMIT 0 ,1"
     })
     X400Bo selectCurrentYanByConstellationId(Long constellationId);
 }
