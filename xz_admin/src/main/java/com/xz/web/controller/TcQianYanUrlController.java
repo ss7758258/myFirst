6ef118a20b2977483ef624cdbc854a21333e7380
JackPeng <keddy8@126.com>
2018-05-20 11:24:20 +0800

    init

--- xz_admin/src/main/java/com/xz/web/controller/TcQianYanUrlController.java
+++ xz_admin/src/main/java/com/xz/web/controller/TcQianYanUrlController.java
@@ -1,24 +1,17 @@
 package com.xz.web.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.xz.web.entity.TcQianYanUrl;
-import com.xz.web.service.TcQianYanUrlService;
-import com.xz.framework.utils.bean.BeanUtil;
-import com.xz.framework.utils.date.DateUtil;
-import com.xz.framework.utils.string.StringUtil;
 import com.xz.framework.common.base.AjaxBean;
 import com.xz.framework.common.base.AjaxStatus;
 import com.xz.framework.common.base.BaseController;
 import com.xz.framework.common.base.PageInfo;
+import com.xz.framework.utils.bean.BeanUtil;
+import com.xz.framework.utils.date.DateUtil;
 import com.xz.web.constant.Constant;
-import java.util.List;
-import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
-import com.xz.framework.utils.files.FileUtil;
+import com.xz.web.entity.TcQianYanUrl;
+import com.xz.web.redis.RedisDao;
+import com.xz.web.service.TcQianYanUrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.stereotype.Controller("tcQianYanUrlTcQianYanUrlController")
 @RequestMapping("tcQianYanUrl")
@@ -26,6 +19,8 @@ public class TcQianYanUrlController extends BaseController {
 
     @Autowired
     private TcQianYanUrlService tcQianYanUrlService;
+    @Autowired
+    private RedisDao redisDao;
 
     
 
@@ -57,6 +52,13 @@ public class TcQianYanUrlController extends BaseController {
         AjaxBean<Integer> ajaxBean = new AjaxBean<Integer>();
         int flag = tcQianYanUrlService.removeById(id);
         if(flag>0) {
+            try
+            {
+                redisDao.del("qianyanUrl");
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
             ajaxBean.setStatus(AjaxStatus.SUCCESS);
             ajaxBean.setMessage("删除成功!");
             ajaxBean.setData(flag);
@@ -86,6 +88,13 @@ public class TcQianYanUrlController extends BaseController {
                 entity.setUpdateTimestamp(DateUtil.getCurrentTimestamp());
                 int flag = tcQianYanUrlService.update(entity);
                 if(flag>0) {
+                    try
+                    {
+                        redisDao.del("qianyanUrl");
+                    }catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
                     ajaxBean.setStatus(AjaxStatus.SUCCESS);
                     ajaxBean.setMessage("更新成功!");
                 }else
--- xz_admin/src/main/java/com/xz/web/controller/TiYanListController.java
+++ xz_admin/src/main/java/com/xz/web/controller/TiYanListController.java
@@ -1,24 +1,23 @@
 package com.xz.web.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.xz.web.entity.TiYanList;
-import com.xz.web.service.TiYanListService;
-import com.xz.framework.utils.bean.BeanUtil;
-import com.xz.framework.utils.date.DateUtil;
-import com.xz.framework.utils.string.StringUtil;
 import com.xz.framework.common.base.AjaxBean;
 import com.xz.framework.common.base.AjaxStatus;
 import com.xz.framework.common.base.BaseController;
 import com.xz.framework.common.base.PageInfo;
-import com.xz.web.constant.Constant;
-import java.util.List;
-import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
+import com.xz.framework.utils.bean.BeanUtil;
+import com.xz.framework.utils.date.DateUtil;
 import com.xz.framework.utils.files.FileUtil;
+import com.xz.framework.utils.string.StringUtil;
+import com.xz.web.constant.Constant;
+import com.xz.web.entity.TiYanList;
+import com.xz.web.redis.RedisDao;
+import com.xz.web.service.TiYanListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @org.springframework.stereotype.Controller("tiYanListTiYanListController")
 @RequestMapping("tiYanList")
@@ -26,6 +25,9 @@ public class TiYanListController extends BaseController {
 
     @Autowired
     private TiYanListService tiYanListService;
+    @Autowired
+    private RedisDao redisDao;
+
 
     @RequestMapping("json/prevPicUpload")
     public
@@ -81,8 +83,22 @@ public class TiYanListController extends BaseController {
     @ResponseBody
     String delTiYanListById(Long id) {
         AjaxBean<Integer> ajaxBean = new AjaxBean<Integer>();
+        TiYanList entity = tiYanListService.getById(id);
+        if(entity==null)
+        {
+            ajaxBean.setStatus(AjaxStatus.ERROR);
+            ajaxBean.setMessage("删除失败!");
+            return this.ajaxJson(ajaxBean);
+        }
         int flag = tiYanListService.removeById(id);
         if(flag>0) {
+            try
+            {
+                redisDao.del("everyDayWord-:" + entity.getConstellationId());
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
             ajaxBean.setStatus(AjaxStatus.SUCCESS);
             ajaxBean.setMessage("删除成功!");
             ajaxBean.setData(flag);
@@ -112,6 +128,13 @@ public class TiYanListController extends BaseController {
                 entity.setUpdateTimestamp(DateUtil.getCurrentTimestamp());
                 int flag = tiYanListService.update(entity);
                 if(flag>0) {
+                    try
+                    {
+                        redisDao.del("everyDayWord-:" + entity.getConstellationId());
+                    }catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
                     ajaxBean.setStatus(AjaxStatus.SUCCESS);
                     ajaxBean.setMessage("更新成功!");
                 }else
