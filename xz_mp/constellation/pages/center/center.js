4a350105bd7d472e71cda64faec7909f794349b9
Vic <824759494@qq.com>
2018-06-01 19:19:17 +0800

    新增：增加个人中心页面

---
+++ xz_mp/constellation/pages/center/center.js
@@ -0,0 +1,21 @@
+Page({
+    data : {
+		navConf : {
+			title : '个人中心',
+			state : 'root',
+			isRoot : false,
+			isIcon : true,
+			iconPath : '',
+            root : '',
+            isTitle : true
+            // root : '/pages/home/home'
+		}
+    },
+    /**
+     * 生命周期初始化组件
+     * @param {any} opts 
+     */
+    onLoad (opts){
+
+    }
+})
