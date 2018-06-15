367b9341fc717a1bffbe1a4979f5e6564cb80b78
Vic <824759494@qq.com>
2018-06-15 18:29:17 +0800

    优化：登录缓存token 格式化代码

--- xz_mp/constellation/app.js
+++ xz_mp/constellation/app.js
@@ -7,57 +7,54 @@ App({
 
 
 
-  onLaunch: function (options) {
-    const _self = this
-    const _SData = this.globalData
-    
-    _SData.userInfo = wx.getStorageSync('userInfo')
-    _self.getLogin().then(res => {
-      console.log(res)
-      wx.setStorage({
-        key: 'token',
-        data: res.token
-      })
-    }).catch(err => {
-      wx.showToast({
-        title: err,
-        icon: 'none'
-      })
-    })
-    _SData.selectConstellation = wx.getStorageSync('selectConstellation') || {id:1,name:"白羊座",time:"3.21-4.19",img:"/assets/images/aries.png",isFirst : true}
-    _SData.userInfo = wx.getStorageSync('userInfo')
-    mta.App.init({
-      "appID": "500613478",
-      "eventID": "500614618", // 高级功能-自定义事件统计ID，配置开通后在初始化处填写
-      "lauchOpts": options, //渠道分析,需在onLaunch方法传入options,如onLaunch:function(options){...}
-      "statPullDownFresh": true, // 使用分析-下拉刷新次数/人数，必须先开通自定义事件，并配置了合法的eventID
-      "statShareApp": true, // 使用分析-分享次数/人数，必须先开通自定义事件，并配置了合法的eventID
-      "statReachBottom": true // 使用分析-页面触底次数/人数，必须先开通自定义事件，并配置了合法的eventID
-    })
-  },
+	onLaunch: function (options) {
+		const _self = this
+		const _SData = this.globalData
 
-  getLogin() {
-    return new utils.Promise((resolve, reject) => {
-      return utils.login().then(res => {
-        api.getLogin({
-          notShowLoading: true,
-          code: res.code
-        }).then(data => {
-          resolve(data)
-        }).catch(err => {
-          reject(err)
-        })
-      })
-    })
-  },
+		_SData.userInfo = wx.getStorageSync('userInfo')
+		_self.getLogin().then(res => {
+			console.log(res)
+			wx.setStorageSync('token', res.token)
+		}).catch(err => {
+			wx.showToast({
+				title: err,
+				icon: 'none'
+			})
+		})
+		_SData.selectConstellation = wx.getStorageSync('selectConstellation') || { id: 1, name: "白羊座", time: "3.21-4.19", img: "/assets/images/aries.png", isFirst: true }
+		_SData.userInfo = wx.getStorageSync('userInfo')
+		mta.App.init({
+			"appID": "500613478",
+			"eventID": "500614618", // 高级功能-自定义事件统计ID，配置开通后在初始化处填写
+			"lauchOpts": options, //渠道分析,需在onLaunch方法传入options,如onLaunch:function(options){...}
+			"statPullDownFresh": true, // 使用分析-下拉刷新次数/人数，必须先开通自定义事件，并配置了合法的eventID
+			"statShareApp": true, // 使用分析-分享次数/人数，必须先开通自定义事件，并配置了合法的eventID
+			"statReachBottom": true // 使用分析-页面触底次数/人数，必须先开通自定义事件，并配置了合法的eventID
+		})
+	},
+
+	getLogin() {
+		return new utils.Promise((resolve, reject) => {
+			return utils.login().then(res => {
+				api.getLogin({
+					notShowLoading: true,
+					code: res.code
+				}).then(data => {
+					resolve(data)
+				}).catch(err => {
+					reject(err)
+				})
+			})
+		})
+	},
 
 
-  globalData: {
-    selectConstellation: null,
-    userInfo: null,
-    lotDetail: {},
-  },
-  //初始化
-  utils,
-  api
+	globalData: {
+		selectConstellation: null,
+		userInfo: null,
+		lotDetail: {},
+	},
+	//初始化
+	utils,
+	api
 })
