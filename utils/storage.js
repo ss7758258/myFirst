9d350d50f17d3cde3c22576b2a4c4ac820840bd5
Carolyn <824759494@qq.com>
2018-07-20 12:44:09 +0800

    修复：修复因Proxy未编译为ES5引发的编译错误

--- utils/storage.js
+++ utils/storage.js
@@ -21,39 +21,39 @@ let storageObj = {
 let callList = []
 
 // 描述器
-let handler = {
-    get: function(obj, prop) {
-        // console.log(`获取【${prop}】的属性值`);
-        return obj[prop];
-    },
-    /**
-     * setter方法监听
-     * @param {*} obj 对象
-     * @param {*} prop 属性
-     * @param {*} value 值
-     */
-    set(obj, prop, value) {
-        // console.log(`【${prop}】 的值变更为：【${JSON.stringify(value)}】`);
-        if(value && value.constructor === Object){
-            // 如果下一级是个对象也添加代理功能
-            obj[prop] = dep(value)
-            return true
-        }
-        // 在当前对象上修改某个属性值的值
-        obj[prop] = value
-        return true
-    }
-}
+// let handler = {
+//     get: function(obj, prop) {
+//         // console.log(`获取【${prop}】的属性值`);
+//         return obj[prop];
+//     },
+//     /**
+//      * setter方法监听
+//      * @param {*} obj 对象
+//      * @param {*} prop 属性
+//      * @param {*} value 值
+//      */
+//     set(obj, prop, value) {
+//         // console.log(`【${prop}】 的值变更为：【${JSON.stringify(value)}】`);
+//         if(value && value.constructor === Object){
+//             // 如果下一级是个对象也添加代理功能
+//             obj[prop] = dep(value)
+//             return true
+//         }
+//         // 在当前对象上修改某个属性值的值
+//         obj[prop] = value
+//         return true
+//     }
+// }
 
 /**
  * 递归添加
  * @param {*} obj
  * @returns
  */
-const dep = (obj) => {
-    return new Proxy(obj,handler)
-}
+// const dep = (obj) => {
+//     return new Proxy(obj,handler)
+// }
 
-let temp = new Proxy(storageObj,handler)
+// let temp = new Proxy(storageObj,handler)
 
-module.exports = temp
+module.exports = storageObj
