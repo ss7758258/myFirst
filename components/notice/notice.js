970794fee8d5aa6001675f3241a641f0ed6787c4
ss <1317569991@qq.com>
2018-07-25 15:01:37 +0800

    公告组件封装中

--- components/notice/notice.js
+++ components/notice/notice.js
@@ -2,18 +2,22 @@ Component({
 
     behaviors: [],
 
-    properties: {
-        myProperty: { // 属性名
-            type: String, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
-            value: '', // 属性初始值（可选），如果未指定则会根据类型选择一个
-            observer: function (newVal, oldVal, changedPath) {
-                // 属性被改变时执行的函数（可选），也可以写成在methods段中定义的方法名字符串, 如：'_propertyChange'
-                // 通常 newVal 就是新设置的数据， oldVal 是旧数据
-            }
+    properties: {},
+    data: {
+        notice:{
+            type:1, //组件类型
+            background:'rgba(0,0,0,0.60)',//背景样式
+            color:'#FFFFFF', //字体颜色
+            time:3000, //轮播时间
+            side:"top",
+            content:['这是一个公才叫我的鸡尾dwdwdwdwdwdwdw酒第五期单位还绝望还件'],//内容
+            src:'',//跳转路径
         },
-        myProperty2: String // 简化的定义方式
-    },
-    data: {}, // 私有数据，可用于模版渲染
+        margin:60,
+        empty:false,
+        left:0,//默认滚动距离
+
+    }, // 私有数据，可用于模版渲染
 
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
     attached: function () { },
@@ -21,21 +25,41 @@ Component({
     detached: function () { },
 
     methods: {
-        onMyButtonTap: function () {
-            this.setData({
-                // 更新属性和数据的方法与更新页面数据的方法类似
-            })
-        },
-        // 内部方法建议以下划线开头
-        _myPrivateMethod: function () {
-            // 这里将 data.A[0].B 设为 'myPrivateData'
-            this.setData({
-                'A[0].B': 'myPrivateData'
-            })
-        },
-        _propertyChange: function (newVal, oldVal) {
+        
+        run(){
+            let self = this,dta=this.data
+            var timer=setInterval(function(){
+                if(-data.margin<data.txt_length){
+                    self.setData({
 
+                    })
+                }
+            },20)
         }
+    },
+    ready() {
+        let screenwidth = wx.getSystemInfoSync().screenWidth //屏幕宽度
+        let self = this 
+        let content = this.data.notice.content, empty = []  // content文本内容 
+        let query = wx.createSelectorQuery().in(this)
+        query.selectAll('.notice').boundingClientRect(res=>{ // 获取文本文字宽度
+            // console.log(res)
+            res.forEach(function(value){
+                self.setData({
+                    screenwidth: screenwidth,
+                    margin:value.width < screenwidth ? screenwidth - value.width : self.data.margin,
+                    txt_length:value.width
+                })
+                
+                this.run()
+            })  
+            // console.log(empty)
+            // self.setData({
+            //     empty:empty
+            // })
+            
+        }).exec()
+        
     }
 
 })
