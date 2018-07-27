e041e6842e4a091d9ca1281917cbf3ff1043974a
Vic <824759494@qq.com>
2018-07-27 20:57:42 +0800

    新增：增加配对页面的静态资源

--- utils/star.js
+++ utils/star.js
@@ -2,97 +2,121 @@ module.exports = {
     1: {
         id: 1,
         name: '白羊座',
-        time: '3.21-4.19',
+        time: '3.21~4.19',
         startTime: '3月21日',
         endTime: '4月19日',
+        bgcolor:'#FFF1D8',
+        color: '#F08000',
         img: '/assets/img/1.svg'
     },
     2: {
         id: 2,
         name: '金牛座',
-        time: '4.20-5.20',
+        time: '4.20~5.20',
         startTime: '4月20日',
         endTime: '5月20日',
+        bgcolor:'#FFF2CA',
+        color: '#FFC400',
         img: '/assets/img/2.svg'
     },
     3: {
         id: 3,
         name: '双子座',
-        time: '5.21-6.21',
+        time: '5.21~6.21',
         startTime: '5月21日',
         endTime: '6月21日',
+        bgcolor:'#FFE8F3',
+        color: '#FF678E',
         img: '/assets/img/3.svg'
     },
     4: {
         id: 4,
         name: '巨蟹座',
-        time: '6.22-7.22',
+        time: '6.22~7.22',
         startTime: '6月22日',
         endTime: '7月22日',
+        bgcolor:'#FFEBEB',
+        color: '#FF6D6D',
         img: '/assets/img/4.svg'
     },
     5: {
         id: 5,
         name: '狮子座',
-        time: '7.23-8.22',
+        time: '7.23~8.22',
         startTime: '7月23日',
         endTime: '8月22日',
+        bgcolor:'#FFEDDD',
+        color: '#FFA200',
         img: '/assets/img/5.svg'
     },
     6: {
         id: 6,
         name: '处女座',
-        time: '8.23-9.22',
+        time: '8.23~9.22',
         startTime: '8月23日',
         endTime: '9月22日',
+        bgcolor:'#FFE9E9',
+        color: '#FF9A95',
         img: '/assets/img/6.svg'
     },
     7: {
         id: 7,
         name: '天秤座',
-        time: '9.23-10.23',
+        time: '9.23~10.23',
         startTime: '9月23日',
         endTime: '10月23日',
+        bgcolor:'#E5F5FF',
+        color: '#5DB5FF',
         img: '/assets/img/7.svg'
     },
     8: {
         id: 8,
         name: '天蝎座',
-        time: '10.24-11.22',
+        time: '10.24~11.22',
         startTime: '10月24日',
         endTime: '11月22日',
+        bgcolor:'#FFEDE2',
+        color: '#9E420F',
         img: '/assets/img/8.svg'
     },
     9: {
         id: 9,
         name: '射手座',
-        time: '11.23-12.21',
+        time: '11.23~12.21',
         startTime: '11月23日',
         endTime: '12月21日',
+        bgcolor:'#FFE8E8',
+        color: '#FF7070',
         img: '/assets/img/9.svg'
     },
     10: {
         id: 10,
         name: '摩羯座',
-        time: '12.22-1.19',
+        time: '12.22~1.19',
         startTime: '12月22日',
         endTime: '1月19日',
+        bgcolor:'#FFECE7',
+        color: '#FF7F52',
         img: '/assets/img/10.svg'
     },
     11: {
         id: 11,
         name: '水瓶座',
-        time: '1.20-2.18',
+        time: '1.20~2.18',
         startTime: '1月20日',
         endTime: '2月18日',
+        bgcolor:'#EDEAFF',
+        color: '#836AFF',
         img: '/assets/img/11.svg'
     },
     12: {
         id: 12,
         name: '双鱼座',
-        time: '2.19-3.20',
+        time: '2.19~3.20',
         startTime: '2月19日',
         endTime: '3月20日',
+        bgcolor:'#E6F0FF',
+        color: '#62A2FF',
         img: '/assets/img/12.svg'
     }
 }
