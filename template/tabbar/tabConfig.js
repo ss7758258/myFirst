a02497cff12a0573a9876cf8224df6f09b26b74f
Vic <824759494@qq.com>
2018-08-08 11:59:20 +0800

    变更：阿拉丁sdk升级 优化：tabbar的配置项

--- template/tabbar/tabConfig.js
+++ template/tabbar/tabConfig.js
@@ -9,6 +9,7 @@ module.exports = {
             icon : '/assets/menu/home.png',
             selectedIcon : '/assets/menu/home-selected.png',
             type : 1,
+            animate : false,
             width : 'auto',
             text : '星座'
         },
@@ -17,6 +18,9 @@ module.exports = {
             icon : '/assets/menu/shake.png',
             selectedIcon : '/assets/menu/shake-selected.png',
             type : 2,
+            animate : true,
+            color : '#F7701F',
+            selectedColor : '#F7701F',
             width : 'auto',
             text : '必玩'
         },
@@ -25,6 +29,7 @@ module.exports = {
             icon : '/assets/menu/yan.png',
             selectedIcon : '/assets/menu/yan-selected.png',
             type : 1,
+            animate : false,
             width : 'auto',
             text : '一言'
         }
--- utils/ald-stat-conf.js
+++ utils/ald-stat-conf.js
@@ -1,5 +1,5 @@
 exports.app_key = "ad2825fecb1b697c0778914596ce16aa"; //请在此行填写从阿拉丁后台获取的appkey
+exports.appid = "wxedc8a06ed85ce4df";
+exports.appsecret = "ff2566625ebe546fe97dc36573aab8ed";
 exports.getLocation = false; //默认不获取用户坐标位置
-exports.appid = ""; //用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.appsecret = "";//用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.defaultPath = 'pages/home/home';//小程序的默认首页, 用于分享时path为空时
+exports.plugin = false;  //您的小程序中是否使用了插件。根据是否启用插件会有不同的接入方式，请参考文档文档。
