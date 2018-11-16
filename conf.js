627a8f020bbae4cc0e7f58634110f211a64daa27
Vic <824759494@qq.com>
2018-11-16 18:19:19 +0800

    重置项目

--- conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    dev : {
-        cdn : 'https://xingzuo-1256217146.file.myqcloud.com'
-    },
-    prod : {
-        cdn : 'https://xingzuo-1256217146.file.myqcloud.com/prod'
-    },
-    appId : 'wxedc8a06ed85ce4df'
-}
--- config.js
+++
@@ -1,4 +0,0 @@
-const wxProcess = require('./wxProcessConf.js')
-wx.wxProcess = wxProcess
-// 使用什么环境
-module.exports = wxProcess.env
